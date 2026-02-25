d3fbc0ca3f1eef6036a8a1d3ccbb754a5f3430c8
Anderson Boyacá <anderson.fbv96@hotmail.com>
2026-02-25 15:38:55 -0500

    Primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-c0f133fb-0805-4bd6-9a18-18a269299868.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
